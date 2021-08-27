eb601a6899e252634a97ec573b74b5e69f2a1078
paulking321 <oluwadamilare321@gmail.com>
2021-08-27 12:50:12 +0100

    Task tracker

---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../others/Home'
+import About from '../others/About'
+
+const routes = [{
+        path: '/',
+        name: 'Home',
+        component: Home,
+    },
+    {
+        path: '/About',
+        name: 'About',
+        component: About,
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+})
+
+export default router
